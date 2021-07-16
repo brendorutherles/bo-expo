0f587bbbd428f6d1737a002696c06bf49015183d
brendorutherles <87491315+brendorutherles@users.noreply.github.com>
2021-07-16 12:18:19 -0300

    deploy inicial
    iniciei o deploy do app expo n git

---
+++ src/helpers/constants.js
@@ -0,0 +1,13 @@
+import  Env from 'react-native-config';
+
+export const MAIN_URL =
+               // 'https://biesp-wgpef.ondigitalocean.app'
+               'https://biesp-2-xi9sc.ondigitalocean.app'
+
+export const API_URL =
+               // 'https://biesp-wgpef.ondigitalocean.app/api/v1'
+               'https://biesp-2-xi9sc.ondigitalocean.app/api/v1'
+
+
+
+export const MAPS_API_KEY = process.env.MAPS_API_KEY || ''
